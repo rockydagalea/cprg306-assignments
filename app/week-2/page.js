007b734cf78452c42812b8f7d0594f07b606a4ef
rockydagalea <134429942+rockydagalea@users.noreply.github.com>
2024-05-27 12:23:34 -0600

    Initial commit with week-2 page and StudentInfo component

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,7 +1,10 @@
+import Student from "./student-info";
+
 export default function Page() {
   return (
     <main>
       <h1>Shopping List</h1>
+      <Student></Student>
     </main>
   );
 }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Student Name: Rocky Van Dagalea</p>
+      <Link href="https://github.com/rockydagalea/cprg306-assignments">
+        https://github.com
+      </Link>
+    </div>
+  );
+}
